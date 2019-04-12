701869ac08c35ea9c665fc65d0dd87b29806944c
sai preetham reddy alladu <alladu.s@husky.neu.edu>
2019-04-12 14:51:00 -0400

    adding

--- src/Business/Organization/RevenueOrganization.java
+++ src/Business/Organization/RevenueOrganization.java
@@ -6,10 +6,23 @@
 
 package Business.Organization;
 
+import Business.Role.AdminRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author Preetham Reddy
  */
-public class RevenueOrganization {
-    
+public class RevenueOrganization extends Organization {
+    public RevenueOrganization(){
+   super(Organization.Type.Revenue.getValue());
+   }
+   
+   @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new AdminRole());
+        return roles;
+    }
 }
