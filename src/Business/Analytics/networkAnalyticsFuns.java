2a3e03941be93d47fc87e36dfeaef91d49f14ea4
ayesh <ayeshreddy22722@gmail.com>
2019-04-25 21:13:38 -0400

    adding total budget pie chart

--- src/Business/Analytics/networkAnalyticsFuns.java
+++ src/Business/Analytics/networkAnalyticsFuns.java
@@ -6,10 +6,8 @@
 package Business.Analytics;
 
 import Business.UserAccount.UserAccount;
-import java.util.ArrayList;
 import java.util.HashMap;
 import Business.EcoSystem;
-import Business.Employee.Employee;
 import Business.Organization.*;
 import Business.Enterprise.Enterprise;
 import Business.Network.Network;
@@ -17,6 +15,7 @@ import Business.Organization.Organization;
 import Business.WorkQueue.BudgetWorkRequest;
 import Business.WorkQueue.WorkRequest;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 /**
  *
  * @author imperio2494
@@ -99,6 +98,57 @@ public class networkAnalyticsFuns {
                             }
                                 for(String k: Allocated_funds.keySet()){
                                     dataset.addValue(Allocated_funds.get(k),k,organization.getName());
+                                }
+                        }
+                    }
+                
+                }
+                  
+                if(temp==network.getName())
+                    return dataset;
+                
+                }
+        
+
+            System.out.print("asdf");
+        return dataset;
+        
+    
+    }
+    public DefaultPieDataset getTotalBudgetdistributionByOrganization(String networkSel){
+        String temp;
+        HashMap<String,HashMap<String,Integer>> ent=null;
+        
+        DefaultPieDataset dataset = new DefaultPieDataset( ); 
+        for(Network network:system.getNetworkList()){
+             ent= new HashMap<String,HashMap<String,Integer>>();
+                if(networkSel==null)
+                    temp=system.getNetworkList().get(0).getName();
+                else
+                    temp=networkSel;
+                //Step 2.a: check against each enterprise
+                for(Enterprise enterprise:network.getEnterpriseDirectory().getEnterpriseList()){
+            
+                    for(Organization organization: enterprise.getOrganizationDirectory().getOrganizationList()){
+                        HashMap<String, Integer> Allocated_funds = new HashMap<>(); 
+                        if(organization instanceof ArmyOrganization || organization instanceof AirForceOrganization || organization instanceof OfficeOfEnviManagementOrganization || organization instanceof OfficeOfNuclearEnergyOrganization){
+                            for(UserAccount userAccount:organization.getUserAccountDirectory().getUserAccountList()){
+                            for (WorkRequest request : userAccount.getWorkQueue().getWorkRequestList()){
+                                    int funds = ((BudgetWorkRequest) request).getTotalBudgetRequest();
+                                    int allocated_funds = ((BudgetWorkRequest) request).getAllocatedBudgetRequest();
+                                    String category = request.getCategory()==null ? "UNN" : request.getCategory();
+
+                                    if(Allocated_funds.get(organization.getName())==null){
+                                        Allocated_funds.put(organization.getName(), allocated_funds);
+                                    }
+                                    else{
+                                        Allocated_funds.put(organization.getName(), Allocated_funds.get(organization.getName())+allocated_funds);
+                                    }
+
+                                }
+                            }
+                                for(String k: Allocated_funds.keySet()){
+                                    dataset.setValue(k,Allocated_funds.get(k));
                                 }
                         }
                     }
